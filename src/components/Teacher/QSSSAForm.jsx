61972d5d0c88d8cac8b5804729e798d216102d48
iApolloBear <aldoespinosaperez1@gmail.com>
2021-09-10 14:46:06 -0500

    Teacher Validation

--- src/components/Teacher/QSSSAForm.jsx
+++ src/components/Teacher/QSSSAForm.jsx
@@ -1,31 +1,63 @@
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+
+const schema = yup.object().shape({
+  topic: yup.string().required(),
+  question: yup.string().required(),
+});
+
 export const QSSSAForm = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
+
+  const onSubmit = (data) => console.log(data);
+
   return (
     <div className="form-main">
-      <div className="form-wrap">
-        <h1>Create a QSSSA</h1>
-        <input
-          className="form-control"
-          type="text"
-          placeholder="Enter your topic name"
-        />
-        <input
-          className="form-control"
-          type="text"
-          placeholder="Enter your question"
-        />
-        <input
-          className="form-control"
-          type="text"
-          placeholder="Enter your Sentence Stem"
-        />
-        <label for="qsssa-image" className="btn btn-primary">
-          <input className="d-none" type="file" name="" id="qsssa-image" />
-          Upload image
-        </label>
-      </div>
-      <div className="btn-main">
-        <button class="btn btn-primary">Create QSSSA</button>
-      </div>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="form-wrap">
+          <h1>Create a QSSSA</h1>
+          <input
+            className="form-control"
+            type="text"
+            placeholder="Enter your topic name"
+            {...register("topic")}
+          />
+          <p style={{ color: "red" }} className="mb-2 mt-0">
+            {errors.topic?.message && "Please enter a valid topic"}
+          </p>
+          <input
+            className="form-control"
+            type="text"
+            placeholder="Enter your question"
+            {...register("question")}
+          />
+          <p style={{ color: "red" }} className="mb-2 mt-0">
+            {errors.question?.message && "Please enter a valid question"}
+          </p>
+          <input
+            className="form-control"
+            type="text"
+            placeholder="Enter your Sentence Stem"
+            {...register("sentenceStem")}
+          />
+          <label htmlFor="qsssa-image" className="btn btn-primary">
+            <input className="d-none" type="file" name="" id="qsssa-image" />
+            Upload image
+          </label>
+        </div>
+        <div className="btn-main">
+          <button type="submit" className="btn btn-primary">
+            Create QSSSA
+          </button>
+        </div>
+      </form>
     </div>
   );
 };
