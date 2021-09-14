2eb540cb85e176cdf708d522e076d76b6ad2404a
iApolloBear <aldoespinosaperez1@gmail.com>
2021-09-14 17:08:08 -0500

    Number Inputs

--- src/components/Teacher/CreateGroupModal.jsx
+++ src/components/Teacher/CreateGroupModal.jsx
@@ -1,7 +1,13 @@
+import { useState } from "react";
 import { Modal } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 export const CreateGroupModal = ({ show, handleClose, id }) => {
+  const [number, setNumber] = useState(0);
+
+  const more = () => setNumber(number + 1);
+  const less = () => setNumber(number - 1);
+
   return (
     <Modal show={show} onHide={handleClose}>
       <Modal.Body>
@@ -12,21 +18,32 @@ export const CreateGroupModal = ({ show, handleClose, id }) => {
               type="number"
               placeholder="Enter number"
               className="custom-input__input form-control"
-              value={0}
+              value={number}
             />
             <button
               type="button"
               className="custom-input__more-button btn-small btn btn-secondary"
+              onClick={more}
             >
               &#9650;
             </button>
             <button
               type="button"
               className="custom-input__less-button btn-small btn btn-secondary"
+              onClick={less}
             >
               &#9660;
             </button>
           </div>
+          {number > 0 &&
+            [...Array(number)].map((item, i) => (
+              <input
+                className="form-control"
+                type="text"
+                placeholder={`Group ${i + 1} name`}
+                value={`Group ${i + 1}`}
+              />
+            ))}
           <input type="checkbox" /> Allow students to know who is in their group
           before they record.
           <br />
