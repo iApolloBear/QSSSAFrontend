b7d82a9beae9390ab0793ed9a897e4c17e804d33
iApolloBear <aldoespinosaperez1@gmail.com>
2021-10-18 10:50:04 -0500

    Production Socket

--- src/context/SocketContext.jsx
+++ src/context/SocketContext.jsx
@@ -8,7 +8,7 @@ export const SocketContext = createContext();
 
 export const SocketProvider = ({ children }) => {
   const { socket, online, connectSocket, joinRoom, getMyGroup } = useSocket(
-    "http://localhost:4000"
+    "https://qsssa-backend.herokuapp.com"
   );
   const { auth } = useContext(AuthContext);
   const { room } = useContext(RoomContext);
