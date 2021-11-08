d7878f7887f6f8970a6a905bdedf80ebb2acbc7e
iApolloBear <aldoespinosaperez1@gmail.com>
2021-11-08 13:35:12 -0600

    Production Socket URI

--- src/context/SocketContext.jsx
+++ src/context/SocketContext.jsx
@@ -6,8 +6,8 @@ export const SocketContext = createContext();
 
 export const SocketProvider = ({ children }) => {
   const { socket, online, connectSocket } = useSocket(
-    //"https://qsssa-backend.herokuapp.com"
-    "http://localhost:4000"
+    "https://qsssa-backend.herokuapp.com"
+    //"http://localhost:4000"
   );
   const { auth } = useContext(AuthContext);
 
